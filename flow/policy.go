5c0d2afdde31ff7b7cfa3d4cd99f13496354651e
Robin Hahling <code@hahling.ch>
2026-01-28 11:25:00 +0100

    flow: pre-allocate policy slice for improved memory allocation
    Signed-off-by: Robin Hahling <code@hahling.ch>

--- flow/policy.go
+++ flow/policy.go
@@ -12,14 +12,15 @@ import (
 
 // Policies generates a list of random policy references.
 func Policies() []*flowpb.Policy {
-	policies := []*flowpb.Policy{}
-	for range rand.Intn(4) {
-		policies = append(policies, &flowpb.Policy{
+	n := rand.Intn(4)
+	policies := make([]*flowpb.Policy, n)
+	for i := range n {
+		policies[i] = &flowpb.Policy{
 			Name:      fake.Name(),
 			Namespace: fake.K8sNamespace(),
 			Labels:    fake.K8sLabels(),
 			Revision:  rand.Uint64() % 100,
-		})
+		}
 	}
 	return policies
 }
