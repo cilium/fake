df075661148e8f52f75a9752648b6779e4a628cb
Alexandre Perrin <alex@isovalent.com>
2026-03-31 18:19:20 +0200

    ci: add more benchmark
    Suggested-by: Robin Hahling <code@hahling.ch>
    Signed-off-by: Alexandre Perrin <alex@isovalent.com>

--- flow/fake_test.go
+++ flow/fake_test.go
@@ -4,29 +4,41 @@
 package flow_test
 
 import (
-	"runtime"
-	"sync"
 	"testing"
 
 	"github.com/cilium/fake/flow"
 )
 
-func BenchmarkFakeFlow(b *testing.B) {
-	// run GOMAXPROCS goroutine, allowing for override when benchmarking while
-	// still being a sane default.
-	p := runtime.GOMAXPROCS(0)
-	n := (b.N / p) + 1 // round up
+func BenchmarkLegacyFlowAPI(b *testing.B) {
+	b.Run("Sequential", func(b *testing.B) {
+		for range b.N {
+			_ = flow.New()
+		}
+	})
 
-	var wg sync.WaitGroup
-	wg.Add(p)
-	for range p {
-		go func() {
+	b.Run("Parallel", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				_ = flow.New()
+			}
+		})
+	})
+}
+
+func BenchmarkInstanceFlowAPI(b *testing.B) {
+	b.Run("Sequential", func(b *testing.B) {
+		f := flow.NewFaker()
+		for range b.N {
+			_ = f.NewFlow()
+		}
+	})
+
+	b.Run("Parallel", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
 			f := flow.NewFaker()
-			for range n {
+			for pb.Next() {
 				_ = f.NewFlow()
 			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
+		})
+	})
 }
